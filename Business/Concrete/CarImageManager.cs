acbe04d6a59599bf16e70dbd6654d94d7360301a
uzeyirbulentsahin <uzeyir.bulent.sahin@gmail.com>
2021-02-27 18:16:43 +0300

    Resimi al Guid isimi verip klasöre kaydet  kaydı 5 adet kontrol et

--- Business/Concrete/CarImageManager.cs
+++ Business/Concrete/CarImageManager.cs
@@ -5,6 +5,7 @@ using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Business.Concrete
@@ -13,21 +14,58 @@ namespace Business.Concrete
     {
         ICarImageDal _carImageDal;
 
+
+
         public CarImageManager(ICarImageDal carImageDal)
         {
             _carImageDal = carImageDal;
+         
         }
 
         public IResult Add(CarImage carImage)
         {
-            //c# guid oluştur
-            //yolundanresmi al
-            //adını değiştir ve yeni kalsore yerleştir
-            //yeni yolunu kaydet
+            if (CarPhotoCount(carImage.CarId ) <= 5)
+            { 
+            
+                ResimiHazirla(carImage);
+
 
             _carImageDal.Add(carImage);
             return new SuccessResult(Messages.EntityAdded);
+            }
+            return new ErrorResult(Messages.EntityCountBig5);
         }
+
+
+        private int CarPhotoCount(int carId) {
+            int imgcount = _carImageDal.GetAll(p => p.CarId == carId).Count;
+            return imgcount;
+        }
+
+
+
+
+        private void ResimiHazirla(CarImage carImage)
+        {
+            
+
+            string GuidKey = Guid.NewGuid().ToString();
+            string getfileName = Path.GetFileName(carImage.ImagePath.ToString());
+            string getDirName = Path.GetDirectoryName(carImage.ImagePath.ToString());
+            string getFileExtension = Path.GetExtension(carImage.ImagePath.ToString());
+            string yeniDosyaAdi = GuidKey + getFileExtension;
+            string yeniYol =Path.Combine("CarImages/", yeniDosyaAdi);
+          
+            carImage.ImagePath = yeniDosyaAdi;
+
+            System.IO.File.Create(yeniYol);
+
+
+
+
+
+        }
+       
 
         public IResult Delete(CarImage carImage)
         {
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,6 +11,6 @@ namespace Business.Constants
         public static string EntityListed = "Kayıtlar Listelendi.";
         public static string EntityDetailListed = "Kayıtlar Listelendi.";
         public static string EntityUpdated = "Kayıt güncellendi.";
-        
+        public static string EntityCountBig5 ="Araba başına 5 resimden fazlası kabul edilemez.";
     }
 }
